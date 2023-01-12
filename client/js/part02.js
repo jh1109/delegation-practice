566278bf4ce21faafbb358531f83cc7584c7f41a
jh1109 <nohjuhee227@gmail.com>
2023-01-12 13:36:53 +0900

    jQuery로 실습해보기

--- client/js/part02.js
+++ client/js/part02.js
@@ -23,7 +23,49 @@ const data = [
   }
 ]
 
+/*
+$('.navigation > li').click(function(e){
+  e.preventDefault();
+  // 이 기능을 통해 a의 기능은 상실됨.
+  console.log($(this));
+  let index = $(this).index();
+  // $(this) 대상자체를 불러온다...
+  // 여기서 index는 data-index가 아니라 this의 index
+
+  $('.navigation > li').removeClass('is-active');
+  $(this).addClass('is-active');
+
+  $('.visual img').attr({
+    'src': `./assets/part01/${data[index].src}`,
+    'alt': data[index].alt
+  });
+})
+*/
+
+// jQuery에서 $는 사실 정말 많은 코드로 짜여진 의미임.
+// 그래서 성능저하를 불러올 수 있음.
+// jQuery를 쓰려면 반드시 $로 node를 불러와야 jQuery를 활용할 수 있음.
 
 
+
+// 이벤트 위임
+$('.navigation').on('click', 'li', function(e){
+  // li를 추가함으로써 위임된거임.(closet 역할을 한거임)
+  // li를 추가하기 전이면 빈자리를 눌러도 li가 나오지만,
+  // li를 추가하면 li만 찾음.
+
+
+  e.preventDefault();
+
+  let index = $(this).attr('data-index');
+  $('.navigation > li').removeClass('is-active');
+  $(this).addClass('is-active');
+
+  $('.visual img').attr({
+    'src': `./assets/part01/${data[index-1].src}`,
+    'alt': data[index-1].alt
+  });
+
+})
 
 
