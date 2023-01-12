1d66c1453adafb491467ed3fe7ab3b4394b7a826
jh1109 <nohjuhee227@gmail.com>
2023-01-12 15:49:40 +0900

    이벤트 위임 sns에 적용해보기 : 좋아요, 댓글달기

--- client/js/part04.js
+++ client/js/part04.js
@@ -1,4 +1,46 @@
+const contents = getNode('.contents');
+const textField = getNode('#comment37');
 
+contents.addEventListener('click', function handler(e){
+  let target = e.target;
+
+  while(!attr(target, 'data-name')){
+    target = target.parentNode;
+
+    if (target.nodeName === 'BODY') {
+      target = null;
+      return;
+    }
+  }
+
+  if(target.dataset.name === 'like'){
+    toggleClass(target, 'active');
+  }
+
+  if(target.dataset.name === 'comment'){
+    textField.focus();
+  }
+
+  if(target.dataset.name === 'send'){
+    textField.value
+
+    let template = `
+    <div class="id">
+    <div class="profile_img border_over"><img src="./assets/part03/tiger.png" alt=""></div>
+        <div class="comment_field">
+            <div class="text_container">
+                <div class="name"><a href="#">노라이언</a></div>
+                <div class="text_field">${textField.value}</div>
+            </div>
+        </div>
+    </div>
+    `
+    insertLast('.comment_container', template);
+    textField.value = null;
+
+  }
+
+})
 
 
 
